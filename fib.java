d95887ddcce9466dafc6ba3cb9fa7b977deb1556
naidubobbili <58201922+naidubobbili@users.noreply.github.com>
2024-02-20 05:24:53 +0530

    Update fib.java

--- fib.java
+++ fib.java
@@ -1,17 +1,15 @@
 import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-	    Scanner s = new Scanner(System.in);
-	    int n = s.nextInt();
-	    int f1=0,f2=1,f3=0;
-	    System.out.print("0 1");
-	    for(int i =2;i<n;i++){
-	        f3=f1+f2;
-	        System.out.print(" "+f3);
-	        f1=f2;
-	        f2=f3;
-	    }
-	    
-	}
-}
+class Main{
+    public static void main(String args []){
+        Scanner s= new Scanner(System.in);
+        int n = s.nextInt();
+        s.close();
+        int f1=1,f2=1,f3=0;
+        System.out.print("0 1 ");
+        for(int i = 2;i<n;i++){
+            f3=f1+f2;
+            f1=f2;
+            f2=f3;
+            System.out.print(f3+" ");
+        }
+    }
