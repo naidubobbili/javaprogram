0bd30e4e1d9708c090e417049f98028fa1846cff
naidubobbili <58201922+naidubobbili@users.noreply.github.com>
2024-02-20 05:54:26 +0530

    Update sort.java

--- sort.java
+++ sort.java
@@ -1,14 +1,14 @@
 import java.util.*;
 class Main{
-    public static void main(String args []){
-        Scanner s= new Scanner(System.in);
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int[] a = new int[n];
-        for(int i =0;i<n;i++){
+        for(int i =0; i<n;i++){
             a[i]=s.nextInt();
         }
         for(int i=0;i<n;i++){
-            for(int j =i;j<n;j++){
+            for(int j = i;j<n;j++){
                 if(a[i]>a[j]){
                     int temp=a[i];
                     a[i]=a[j];
@@ -19,6 +19,6 @@ class Main{
         for(int i =0;i<n;i++){
             System.out.print(a[i]+" ");
         }
-     
+        
     }
 }
