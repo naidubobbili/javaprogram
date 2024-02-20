89a532908685f3098966e04ea0ea0adc9c3be5a8
naidubobbili <58201922+naidubobbili@users.noreply.github.com>
2024-02-20 05:41:59 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main{
+    public static void main(String args []){
+        Scanner s= new Scanner(System.in);
+        int n = s.nextInt();
+        int[] a = new int[n];
+        for(int i =0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            for(int j =i;j<n;j++){
+                if(a[i]>a[j]){
+                    int temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        for(int i =0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+     
+    }
+}
